b4d52fa0e30e6370d25e1e50064a92c8fa2312c7
rixynx <rixynx@gmail.com>
2025-04-10 09:05:23 -0400

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -8,8 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bullet extends Actor
 {
-    int team;
-    int beam;
+    private int team;
+    private int beam;
     public Bullet(int team){
         this.team = team;
         if(team == 1) beam = 2;
