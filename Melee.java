86b724407eef39ddc261ba104f5e533c043da1e2
rixynx <rixynx@gmail.com>
2025-04-23 21:26:00 -0400

    Update Melee.java

--- Melee.java
+++ Melee.java
@@ -9,21 +9,40 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public abstract class Melee extends Chara
 {
+    protected GreenfootImage characterImage;
+    private GreenfootImage originalImage;
+    private boolean isSpinning = false;
     protected int atk = 90;
     protected int range = 97;
     public Melee(int team, int pos){
         super(team,pos);
+        originalImage = getImage();
     }
     public void act()
     {
-        
         super.act();
-        attackspeed();
+        if(isSpinning){
+            spinAnimation();
+        }else{
+            attackspeed();
+        }
     }
     protected void attack(){
         if(!inRange) return;
-        if(atk==0){
+        if(atk==0 && !isSpinning){
             closest.dealDamage(damage);
+            isSpinning = true;
+            spinFrames = 0;
+        }
+    }
+    private void spinAnimation() {
+        spinFrames++;
+        setRotation(getRotation() + 12);
+        if(spinFrames >= 30){
+            isSpinning = false;
+            setRotation(0);
+            atk = atkspeed;
+            setImage(originalImage);
         }
     }
     private void attackspeed(){
